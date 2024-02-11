5a785ab1d131f95988a743b76660ba897124e138
guidn <guioliveira852963@gmail.com>
2024-02-11 15:11:49 -0300

    finalizacao calculadogoogle

--- scripts.js
+++ scripts.js
@@ -50,13 +50,13 @@ function CalcGoogle() {
     else if (opcao == "Comprimento") {
         var resultado = (valor1 / 100)
     }
-    else if (opcao == "Armazenamento de Dados" || opcao == "Transmissao de Dados") {
+    else if (opcao == "Armazenamento de Dados" || opcao == "Transmissao de Dados" || opcao == "Frequencia") {
         var resultado = (valor1 / 1000)
     }
     else if (opcao == "Tempo") {
         var resultado = (valor1 / 60)
     }
-    else if (opcao == "Massa") {
+    else if (opcao == "Massa" || opcao == "Volume") {
         var resultado = (valor1 * 1000)
     }
     else if (opcao == "Energia Mecanica") {
@@ -65,6 +65,19 @@ function CalcGoogle() {
     else if (opcao == "Consumo de Combustivel") {
         var resultado = (valor1 * 2.352)
     }
+    else if (opcao == "Velocidade") {
+        var resultado = (valor1 * 3.6)
+    }
+    else if (opcao == "Pressao") {
+        var resultado = (valor1 * 1.013)
+    }
+    else if (opcao == "Area") {
+        var resultado = (valor1 * (1 * 10 ** 6))
+    }
+    else if (opcao == "Angulo") {
+        var resultado = (valor1 * (Math.PI / 180))
+    }
+    
     return alert(resultado)
 }
 
